0995bf7180354fe28438de6b2a0c538b04844c29
ibrahim-elshorbagy <e.mohmed55@gmail.com>
2024-09-07 20:50:21 +0200

    user Projects

---
+++ database/migrations/2024_09_07_184626_create_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('portfolio_id')->constrained('portfolios')->references('user_id')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->string('project_category');
+            $table->string('media_files');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
