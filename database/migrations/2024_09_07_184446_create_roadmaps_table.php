d252e2b834bbedc7260c4a807e642e79f7cbf449
ibrahim-elshorbagy <e.mohmed55@gmail.com>
2024-09-07 20:46:17 +0200

    roadmap + courses

---
+++ database/migrations/2024_09_07_184446_create_roadmaps_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roadmaps', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('category');
+            $table->string('duration');
+            $table->integer('price');
+            $table->integer('enrollment_count')->default(0);
+            $table->foreignId('instructor_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roadmaps');
+    }
+};
